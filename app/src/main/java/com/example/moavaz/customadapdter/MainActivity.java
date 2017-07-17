2fcac4f935dd088684e80f62355b378a117ea73d
Moavaz <ezioauditore144@gmail.com>
2017-07-18 03:58:44 +0500

    Initial commit

---
+++ app/src/main/java/com/example/moavaz/customadapdter/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.moavaz.customadapdter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] student = {"ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem","ali", "hasan", "hamza", "usman", "yousaf", "bilal", "naseem"};
+        ListAdapter listAdapter = new Custom_adapter(this,student);
+        ListView listView = (ListView) findViewById(R.id.ListView);
+        listView.setAdapter(listAdapter);
+
+    }
+}
